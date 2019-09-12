93c4b0c227dfc7cf09729ff492d1484ecb21da94
Mincheng Li <minchengli23@gmail.com>
2019-09-12 17:11:37 +1000

    no message

--- src/authentication/LoginServlet.java
+++ src/authentication/LoginServlet.java
@@ -25,31 +25,15 @@ public class LoginServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-    
-    public void init(ServletConfig servletconfig) throws ServletException
-    {
-    		usernamed = servletconfig.getInitParameter("userNameI");
-    		passwordd = servletconfig.getInitParameter("passWordI");
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String username = request.getParameter("UserName");
-		String password = request.getParameter("passWord");
 		response.setContentType("text/html");
-		System.out.println("Hello from GET method");
 		PrintWriter writer = response.getWriter();
-		if (username.equals(usernamed) && password.equals(passwordd))
-		{
-			writer.println("<h3> Hello</h3>");
-		}
-		else
-		{
-			writer.println("<h3> Error</h3>");
-		}
+		writer.println("<h3> hello world HHHHHHHH</h3>");
 	}
 
 	/**
@@ -57,19 +41,7 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String username = request.getParameter("username");
-		String password = request.getParameter("passWord");
-		response.setContentType("text/html");
-		System.out.println("Hello from GET method");
-		PrintWriter writer = response.getWriter();
-		if (username.equals(usernamed) && password.equals(passwordd))
-		{
-			response.sendRedirect("success.jsp");
-		}
-		else
-		{
-			writer.println("<h3> Error</h3>");
-		}
+		doGet(request, response);
 	}
 
 }
